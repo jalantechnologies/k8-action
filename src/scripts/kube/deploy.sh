7c7590dcf994afd941a0f197311dff8c3939fa44
bbbneo333 <ankit@jalantechnologies.com>
2022-05-27 11:28:58 +0530

    fix: conditional expression for analyze step, doppler is now optional

--- src/scripts/kube/deploy.sh
+++ src/scripts/kube/deploy.sh
@@ -2,7 +2,7 @@
 
 # requires - kubectl, doppler
 # requires - KUBE_NS, KUBE_APP, KUBE_ENV, KUBE_DEPLOYMENT_IMAGE, KUBE_INGRESS_HOSTNAME
-# requires - DOPPLER_TOKEN, DOPPLER_PROJECT, DOPPLER_CONFIG
+# optional - DOPPLER_TOKEN, DOPPLER_PROJECT, DOPPLER_CONFIG
 
 echo "deploying to k8"
 echo "kube namespace - $KUBE_NS"
@@ -15,8 +15,12 @@ echo "kube ingress hostname - $KUBE_INGRESS_HOSTNAME"
 kubectl get namespace $KUBE_NS || kubectl create namespace $KUBE_NS
 
 # create secrets which will be consumed by the deployment using environment variables
+
 kubectl -n $KUBE_NS delete secret $KUBE_APP-env-vars || true
-kubectl -n $KUBE_NS create secret generic $KUBE_APP-env-vars --from-env-file <(doppler secrets download --no-file --format docker --project $DOPPLER_PROJECT --config $DOPPLER_CONFIG)
+
+if [[ -n "$DOPPLER_TOKEN" ]]; then
+  kubectl -n $KUBE_NS create secret generic $KUBE_APP-env-vars --from-env-file <(doppler secrets download --no-file --format docker --project $DOPPLER_PROJECT --config $DOPPLER_CONFIG)
+fi
 
 # apply kube config (shared / env)
 
