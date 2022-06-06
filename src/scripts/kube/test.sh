103c6407a38b781b44d6c0879e7835753267efcb
bbbneo333 <ankit@jalantechnologies.com>
2022-06-06 11:21:13 +0530

    fix: removed support for configurable lint and test command

--- src/scripts/kube/test.sh
+++ src/scripts/kube/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# requires - DOCKER_IMAGE, DOCKER_COMMAND
+# requires - DOCKER_IMAGE
 
 if [ -f "docker-compose.test.yml" ]; then
     docker-compose -f docker-compose.test.yml up --exit-code-from app
 else
-    docker run -t "$DOCKER_IMAGE" "$DOCKER_COMMAND"
+    docker run -t "$DOCKER_IMAGE" npm test
 fi
