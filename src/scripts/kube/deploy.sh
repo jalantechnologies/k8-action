d29d1a333b2f299627dafed642d3ded78f3d9560
bbbneo333 <ankit@jalantechnologies.com>
2022-06-06 13:34:28 +0530

    fix: kube resources are now not created with a namespace

--- src/scripts/kube/deploy.sh
+++ src/scripts/kube/deploy.sh
@@ -36,13 +36,13 @@ kube_env_dir="lib/kube/$KUBE_ENV"
 
 if [ -d "$kube_shared_dir" ]; then
     for file in "$kube_shared_dir"/*; do
-        envsubst <"$file" | kubectl -n "$KUBE_NS" apply -f -
+        envsubst <"$file" | kubectl apply -f -
     done
 fi
 
 if [ -d "$kube_env_dir" ]; then
     for file in "$kube_env_dir"/*; do
-        envsubst <"$file" | kubectl -n "$KUBE_NS" apply -f -
+        envsubst <"$file" | kubectl apply -f -
     done
 fi
 
