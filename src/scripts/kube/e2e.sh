6f7e6e2763a1ca66312ff47ec8d60caea1950947
Akash Lakhera <akashlakhera05@gmail.com>
2022-09-02 12:20:15 +0530

    End to end tests should use e2e:ci command instead of just e2e

--- src/scripts/kube/e2e.sh
+++ src/scripts/kube/e2e.sh
@@ -5,5 +5,5 @@
 if [ -f "docker-compose.e2e.yml" ]; then
     docker-compose -f docker-compose.e2e.yml up --exit-code-from app
 else
-    docker run -t "$DOCKER_IMAGE" npm run e2e
+    docker run -t "$DOCKER_IMAGE" npm run e2e:ci
 fi
