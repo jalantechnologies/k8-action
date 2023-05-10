2732d90a8c9cfc748c5eae2cde23f67b0e7151d0
Ankit Bhawnani <bbbneo333@gmail.com>
2023-05-10 19:40:51 +0530

    chore: changed --exit-code-from app to --abort-on-container-exit for e2e

--- src/scripts/kube/e2e.sh
+++ src/scripts/kube/e2e.sh
@@ -3,7 +3,7 @@
 # requires - DOCKER_IMAGE
 
 if [ -f "docker-compose.e2e.yml" ]; then
-    docker-compose -f docker-compose.e2e.yml up --exit-code-from app
+    docker-compose -f docker-compose.e2e.yml up --abort-on-container-exit
 else
     docker run -t "$DOCKER_IMAGE" npm run e2e
 fi
